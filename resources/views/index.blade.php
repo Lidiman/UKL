37b2bc88cbab568a3e1e936e5602b0a200b02bc8
Lidiman <Bamaground@gmail.com>
2026-01-17 00:44:08 +0700

    Add Test file

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div style="text-align:center; margin-top:50px;">
+        <h1>Laravel is working!</h1>
+        <p>Welcome to your Laravel application.</p>
+        <p>Current time: {{ now() }}</p>
+    </div>
+</body>
+</html>
